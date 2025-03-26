7480e8ecf981903165257ade0fa8b90bf760d29f
김연진 <love_homebody@naver.com>
2025-03-26 23:58:37 +0900

    feat: add internal call

--- src/main/java/com/onseju/orderservice/order/client/UserServiceClient.java
+++ src/main/java/com/onseju/orderservice/order/client/UserServiceClient.java
@@ -1,36 +1,15 @@
 package com.onseju.orderservice.order.client;
 
-import java.util.HashMap;
-import java.util.Map;
-
-import org.springframework.http.HttpEntity;
-import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
-
 import com.onseju.orderservice.order.dto.BeforeTradeOrderDto;
+import com.onseju.orderservice.order.dto.OrderValidationResponse;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
+@FeignClient(name = "user-service", url = "${user-service.url}")
+public interface UserServiceClient {
 
-@Slf4j
-@Service
-@RequiredArgsConstructor
-public class UserServiceClient {
-
-	private final RestTemplate restTemplate;
-
-	private final String userServiceUrl = "http://localhost:8083";
-
-	public void validateAccountAndHoldings(final BeforeTradeOrderDto dto) {
-		final String url = userServiceUrl + "/api/user-service/validate";
-		Map<String, Object> payload = new HashMap<>();
-		payload.put("companyCode", dto.companyCode());
-		payload.put("type", dto.type());
-		payload.put("totalQuantity", dto.totalQuantity());
-		payload.put("price", dto.price());
-		payload.put("accountId", dto.accountId());
-
-		HttpEntity<Map<String, Object>> request = new HttpEntity<>(payload);
-		restTemplate.postForEntity(url, request, Void.class);
-	}
+	@PostMapping("/api/user-service/validate")
+	ResponseEntity<OrderValidationResponse> validateOrderAndGetAccountId(@RequestBody BeforeTradeOrderDto request);
 }
