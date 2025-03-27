6b7050bfb82e6c2beb3b565fbf8c71c1d806bbb4
HminiL <hmin10580@gmail.com>
2025-03-27 12:42:03 +0900

    feat: remove default value in rabbitMQ config

--- src/main/java/com/onseju/orderservice/global/config/RabbitMQConfig.java
+++ src/main/java/com/onseju/orderservice/global/config/RabbitMQConfig.java
@@ -67,13 +67,13 @@ public class RabbitMQConfig {
 
 	private static final long MESSAGE_TTL = 10000; // 10초
 
-	@Value("${spring.rabbitmq.host:localhost}")
+	@Value("${spring.rabbitmq.host}")
     private String host;
-    @Value("${spring.rabbitmq.port:5672}")
+    @Value("${spring.rabbitmq.port}")
     private int port;
-    @Value("${spring.rabbitmq.username:admin}")
+    @Value("${spring.rabbitmq.username}")
     private String username;
-    @Value("${spring.rabbitmq.password:admin123}")
+    @Value("${spring.rabbitmq.password}")
     private String password;
 
     @Bean
