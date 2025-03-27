e7e1c585a8426b77f0c961626f43b5d0b304ac0a
platypus3036 <akakehcn@gmail.com>
2025-03-27 15:21:17 +0900

    fix : test code uncommenting

--- src/test/java/com/onseju/orderservice/fake/FakeTradeHistoryRepository.java
+++ src/test/java/com/onseju/orderservice/fake/FakeTradeHistoryRepository.java
@@ -1,50 +1,50 @@
-// package com.onseju.orderservice.fake;
-//
-// import java.util.Comparator;
-// import java.util.concurrent.ConcurrentSkipListSet;
-//
-// import com.onseju.orderservice.tradehistory.domain.TradeHistory;
-// import com.onseju.orderservice.tradehistory.service.repository.TradeHistoryRepository;
-//
-// public class FakeTradeHistoryRepository implements TradeHistoryRepository {
-//
-// 	private final ConcurrentSkipListSet<TradeHistory> elements = new ConcurrentSkipListSet<>(
-// 			Comparator.comparing(TradeHistory::getId));
-//
-// 	@Override
-// 	public TradeHistory save(TradeHistory tradeHistory) {
-// 		if (hasElement(tradeHistory)) {
-// 			elements.stream()
-// 					.filter(element -> element.getId().equals(tradeHistory.getId()))
-// 					.forEach(elements::remove);
-// 			elements.add(tradeHistory);
-// 			return tradeHistory;
-// 		}
-// 		TradeHistory saved = TradeHistory.builder()
-// 				.id((long)(elements.size() + 1))
-// 				.companyCode(tradeHistory.getCompanyCode())
-// 				.sellOrderId(tradeHistory.getSellOrderId())
-// 				.buyOrderId(tradeHistory.getBuyOrderId())
-// 				.price(tradeHistory.getPrice())
-// 				.quantity(tradeHistory.getQuantity())
-// 				.tradeTime(tradeHistory.getTradeTime())
-// 				.build();
-// 		elements.add(saved);
-// 		return saved;
-// 	}
-//
-// 	private boolean hasElement(TradeHistory tradeHistory) {
-// 		if (tradeHistory.getId() == null) {
-// 			return false;
-// 		}
-// 		return elements.stream()
-// 				.anyMatch(o -> o.getId().equals(tradeHistory.getId()));
-// 	}
-//
-// 	public TradeHistory findById(long id) {
-// 		return elements.stream()
-// 				.filter(o -> o.getId().equals(id))
-// 				.findAny()
-// 				.orElse(null);
-// 	}
-// }
+package com.onseju.orderservice.fake;
+
+import java.util.Comparator;
+import java.util.concurrent.ConcurrentSkipListSet;
+
+import com.onseju.orderservice.tradehistory.domain.TradeHistory;
+import com.onseju.orderservice.tradehistory.service.repository.TradeHistoryRepository;
+
+public class FakeTradeHistoryRepository implements TradeHistoryRepository {
+
+	private final ConcurrentSkipListSet<TradeHistory> elements = new ConcurrentSkipListSet<>(
+			Comparator.comparing(TradeHistory::getId));
+
+	@Override
+	public TradeHistory save(TradeHistory tradeHistory) {
+		if (hasElement(tradeHistory)) {
+			elements.stream()
+					.filter(element -> element.getId().equals(tradeHistory.getId()))
+					.forEach(elements::remove);
+			elements.add(tradeHistory);
+			return tradeHistory;
+		}
+		TradeHistory saved = TradeHistory.builder()
+				.id((long)(elements.size() + 1))
+				.companyCode(tradeHistory.getCompanyCode())
+				.sellOrderId(tradeHistory.getSellOrderId())
+				.buyOrderId(tradeHistory.getBuyOrderId())
+				.price(tradeHistory.getPrice())
+				.quantity(tradeHistory.getQuantity())
+				.tradeTime(tradeHistory.getTradeTime())
+				.build();
+		elements.add(saved);
+		return saved;
+	}
+
+	private boolean hasElement(TradeHistory tradeHistory) {
+		if (tradeHistory.getId() == null) {
+			return false;
+		}
+		return elements.stream()
+				.anyMatch(o -> o.getId().equals(tradeHistory.getId()));
+	}
+
+	public TradeHistory findById(long id) {
+		return elements.stream()
+				.filter(o -> o.getId().equals(id))
+				.findAny()
+				.orElse(null);
+	}
+}
--- src/test/java/com/onseju/orderservice/tradehistory/service/TradeHistoryServiceTest.java
+++ src/test/java/com/onseju/orderservice/tradehistory/service/TradeHistoryServiceTest.java
@@ -1,53 +1,53 @@
-// package com.onseju.orderservice.tradehistory.service;
-//
-// import static org.assertj.core.api.Assertions.*;
-//
-// import java.math.BigDecimal;
-// import java.time.LocalDateTime;
-// import java.time.ZoneOffset;
-//
-// import org.junit.jupiter.api.BeforeEach;
-// import org.junit.jupiter.api.DisplayName;
-// import org.junit.jupiter.api.Test;
-//
-// import com.onseju.orderservice.events.mapper.TradeHistoryMapper;
-// import com.onseju.orderservice.fake.FakeTradeHistoryRepository;
-// import com.onseju.orderservice.tradehistory.domain.TradeHistory;
-// import com.onseju.orderservice.tradehistory.dto.TradeHistoryResponse;
-//
-// class TradeHistoryServiceTest {
-//
-// 	private FakeTradeHistoryRepository tradeHistoryRepository;
-// 	private TradeHistoryMapper tradeHistoryMapper = new TradeHistoryMapper();
-// 	private TradeHistoryService tradeHistoryService;
-//
-// 	@BeforeEach
-// 	void setUp() {
-// 		tradeHistoryRepository = new FakeTradeHistoryRepository();
-// 		tradeHistoryService = new TradeHistoryService(tradeHistoryRepository, tradeHistoryMapper);
-// 	}
-//
-// 	@Test
-// 	@DisplayName("체결 내역을 저장한다.")
-// 	void save() {
-// 		// given
-// 		TradeHistoryResponse tradeHistoryResponse = new TradeHistoryResponse(
-// 				"005930",
-// 				1L,
-// 				2L,
-// 				new BigDecimal(100),
-// 				new BigDecimal(1000),
-// 				LocalDateTime.of(2025, 01, 01, 0, 0).toEpochSecond(ZoneOffset.UTC));
-//
-// 		// when
-// 		tradeHistoryService.saveTradeHistory(tradeHistoryResponse);
-//
-// 		// then
-// 		TradeHistory saved = tradeHistoryRepository.findById(1L);
-// 		assertThat(saved).isNotNull();
-// 		assertThat(saved.getCompanyCode()).isEqualTo(tradeHistoryResponse.companyCode());
-// 		assertThat(saved.getSellOrderId()).isEqualTo(tradeHistoryResponse.sellOrderId());
-// 		assertThat(saved.getBuyOrderId()).isEqualTo(tradeHistoryResponse.buyOrderId());
-// 		assertThat(saved.getTradeTime()).isEqualTo(tradeHistoryResponse.tradeTime());
-// 	}
-// }
+package com.onseju.orderservice.tradehistory.service;
+
+import static org.assertj.core.api.Assertions.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import com.onseju.orderservice.events.mapper.TradeHistoryMapper;
+import com.onseju.orderservice.fake.FakeTradeHistoryRepository;
+import com.onseju.orderservice.tradehistory.domain.TradeHistory;
+import com.onseju.orderservice.tradehistory.dto.TradeHistoryResponse;
+
+class TradeHistoryServiceTest {
+
+	private FakeTradeHistoryRepository tradeHistoryRepository;
+	private TradeHistoryMapper tradeHistoryMapper = new TradeHistoryMapper();
+	private TradeHistoryService tradeHistoryService;
+
+	@BeforeEach
+	void setUp() {
+		tradeHistoryRepository = new FakeTradeHistoryRepository();
+		tradeHistoryService = new TradeHistoryService(tradeHistoryRepository, tradeHistoryMapper);
+	}
+
+	@Test
+	@DisplayName("체결 내역을 저장한다.")
+	void save() {
+		// given
+		TradeHistoryResponse tradeHistoryResponse = new TradeHistoryResponse(
+				"005930",
+				1L,
+				2L,
+				new BigDecimal(100),
+				new BigDecimal(1000),
+				LocalDateTime.of(2025, 01, 01, 0, 0).toEpochSecond(ZoneOffset.UTC));
+
+		// when
+		tradeHistoryService.saveTradeHistory(tradeHistoryResponse);
+
+		// then
+		TradeHistory saved = tradeHistoryRepository.findById(1L);
+		assertThat(saved).isNotNull();
+		assertThat(saved.getCompanyCode()).isEqualTo(tradeHistoryResponse.companyCode());
+		assertThat(saved.getSellOrderId()).isEqualTo(tradeHistoryResponse.sellOrderId());
+		assertThat(saved.getBuyOrderId()).isEqualTo(tradeHistoryResponse.buyOrderId());
+		assertThat(saved.getTradeTime()).isEqualTo(tradeHistoryResponse.tradeTime());
+	}
+}
