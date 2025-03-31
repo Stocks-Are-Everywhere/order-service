ad82b2a4283b3f016ff03753e7b20361f42a966a
Hugh <lee2317884@gmail.com>
2025-03-31 22:34:33 +0900

    refactor: modify securityConfig

--- src/main/java/com/onseju/orderservice/order/controller/OrderController.java
+++ src/main/java/com/onseju/orderservice/order/controller/OrderController.java
@@ -1,20 +1,19 @@
 package com.onseju.orderservice.order.controller;
 
-import com.onseju.orderservice.global.response.ApiResponse;
-import com.onseju.orderservice.global.security.UserDetailsImpl;
-import com.onseju.orderservice.order.controller.request.OrderRequest;
-import com.onseju.orderservice.order.controller.resposne.OrderResponse;
-import com.onseju.orderservice.order.dto.BeforeTradeOrderDto;
-import com.onseju.orderservice.order.service.OrderService;
-import lombok.RequiredArgsConstructor;
-
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.onseju.orderservice.global.response.ApiResponse;
+import com.onseju.orderservice.order.controller.request.OrderRequest;
+import com.onseju.orderservice.order.controller.resposne.OrderResponse;
+import com.onseju.orderservice.order.dto.BeforeTradeOrderDto;
+import com.onseju.orderservice.order.service.OrderService;
+
+import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequestMapping("/api/order")
@@ -25,17 +24,17 @@ public class OrderController {
 
 	@PostMapping
 	public ResponseEntity<ApiResponse<OrderResponse>> received(
-			@RequestBody final OrderRequest request,
-			@AuthenticationPrincipal final UserDetailsImpl user
+			@RequestBody final OrderRequest request
+			// @AuthenticationPrincipal final UserDetailsImpl user
 	) {
 		final BeforeTradeOrderDto dto = BeforeTradeOrderDto.builder()
 				.companyCode(request.companyCode())
 				.type(request.type().name())
 				.totalQuantity(request.totalQuantity())
 				.price(request.price())
-				.memberId(user.getMember().getId())
+				.memberId(request.memberId())
 				.build();
-			ApiResponse<OrderResponse> response = orderService.placeOrder(dto);
+		ApiResponse<OrderResponse> response = orderService.placeOrder(dto);
 		return ResponseEntity.status(HttpStatus.OK).body(response);
 	}
 }
--- src/main/java/com/onseju/orderservice/order/controller/request/OrderRequest.java
+++ src/main/java/com/onseju/orderservice/order/controller/request/OrderRequest.java
@@ -1,15 +1,17 @@
 package com.onseju.orderservice.order.controller.request;
 
-import com.onseju.orderservice.order.domain.Type;
-import lombok.Builder;
-
 import java.math.BigDecimal;
+
+import com.onseju.orderservice.order.domain.Type;
+
+import lombok.Builder;
 
 @Builder
 public record OrderRequest(
 		String companyCode,
 		Type type,
 		BigDecimal totalQuantity,
-		BigDecimal price
+		BigDecimal price,
+		Long memberId
 ) {
 }
